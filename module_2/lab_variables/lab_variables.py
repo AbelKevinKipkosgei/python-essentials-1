3b37e94731c538d2c96d280c8c55b7045c96db9d
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2026-02-16 08:19:05 +0300

    learn(variables): work on variables lab

---
+++ module_2/lab_variables/lab_variables.py
@@ -0,0 +1,9 @@
+john = 3
+mary = 5
+adam = 6
+
+print(f"{john}, {mary}, {adam}")
+print(john, mary, adam, sep=",")
+
+total_apples = john + mary + adam
+print(f"Total number of apples: {total_apples}")
